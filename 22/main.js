8e8a644d9d71315a0330f5388a5f8db67ad4a7a6
Yana15082007 <yana1508reva888@gmail.com>
2026-06-17 23:28:20 +0200

    hw23

--- 22/main.js
+++ 22/main.js
@@ -1,112 +1,117 @@
-const counter1 = (function () {
-  let count = 0;
+console.log('#5. JavaScript homework example file')
 
-  return function (n) {
-    if (n !== undefined) {
-      count = n;
-      return count;
-    }
-    return count++;
-  };
-})();
+/*
+ * #1
+ *
+ * Створіть функцію counter(), яка має реалізувати лічильник за допомогою замикання:
+ * функція може приймати число як аргумент counter(n)
+ * якщо число передано у функцію - лічба починається із зазначеного числа
+ * якщо ні - то лічба триває
+ */
 
-console.log(counter1());
-console.log(counter1());
-console.log(counter1(100));
-console.log(counter1());
-console.log(counter1());
-console.log(counter1(500));
-console.log(counter1());
-console.log(counter1());
-console.log(counter1(0));
-console.log(counter1());
-console.log(counter1());
+// const counter = function() {}
 
-const counterFactory = function () {
-  let count = 0;
+// console.log(counter()) // 0
+// console.log(counter()) // 1
+// console.log(counter(100)) // 100
+// console.log(counter()) // 101
+// console.log(counter()) // 102
+// console.log(counter(500)) // 500
+// console.log(counter()) // 501
+// console.log(counter()) // 502
+// console.log(counter(0)) // 0
+// console.log(counter()) // 0
+// console.log(counter()) // 1
 
-  return {
-    value(n) {
-      if (n !== undefined) {
-        count = n;
-      }
-      return count;
-    },
+/*
+ * #2
+ *
+ * Створіть функцію counterFactory, яка має реалізувати три методи за допомогою замикання:
+ * початкове значення лічильника - 0
+ * counterFactory.value() - повертає значення лічильника
+ * counterFactory.value(n) - встановлює значення лічильника, повертає нове значення
+ * counterFactory.increment() - збільшує значення лічильника на 1
+ * counterFactory.decrement() - зменшує значення лічильника на 1
+ */
 
-    increment() {
-      count++;
-    },
+// const counterFactory = function () {}
 
-    decrement() {
-      count--;
-    },
-  };
-};
+// console.log(counterFactory.value()) // 0
+// counterFactory.increment()
+// counterFactory.increment()
+// counterFactory.increment()
+// console.log(counterFactory.value()) // 3
+// counterFactory.decrement()
+// counterFactory.decrement()
+// console.log(counterFactory.value()) // 1
+// console.log(counterFactory.value(100)) // 100
+// counterFactory.decrement()
+// console.log(counterFactory.value()) // 99
+// console.log(counterFactory.value(200)) // 200
+// counterFactory.increment()
+// console.log(counterFactory.value()) // 201
 
-const counter = counterFactory();
+/*
+ * #3
+ *
+ * Створіть функцію myPow(a, b, myPrint). Всередині реалізуйте рекурсію для підрахунку результату піднесення числа a до ступеня b.
+ * - Функція myPrint(a, b, res) - глобальна функція, що має генерувати з параметрів a, b, res рядок вигляду 'a^b=res' і повертати його.
+ * - myPrint() має бути передана в myPow() як параметр і викликана всередині як callback-функція.
+ * - функція myPow() як значення, що повертається, приймає результат myPrint().
+ * Наприклад:
+ * console.log(myPow(3, 4, myPrint)); // 3^4=81
+ * console.log(myPow(2, 3, myPrint)); // 2^3=8
+ * console.log(myPow(2, 0, myPrint))  // 2^0=1
+ * console.log(myPow(2, -2, myPrint)) // 2^-2=0.25
+ */
 
-console.log(counter.value());
-counter.increment();
-counter.increment();
-counter.increment();
-console.log(counter.value());
+// const myPrint = () => {}
+// const myPow = () => {}
 
-counter.decrement();
-counter.decrement();
-console.log(counter.value());
+// console.log(myPow(3, 4, myPrint)) // 3^4=81
+// console.log(myPow(2, 3, myPrint)) // 2^3=8
+// console.log(myPow(2, 0, myPrint)) // 2^0=1
+// console.log(myPow(2, -2, myPrint)) // 2^-2=0.25
 
-console.log(counter.value(100));
-counter.decrement();
-console.log(counter.value());
 
-console.log(counter.value(200));
-counter.increment();
-console.log(counter.value());
+/*
+ * #4
+ * Створіть функцію myMax(arr), яка як параметр приймає
+ * довільний числовий масив і повертає максимальне число з переданого їй масиву.
+ * У реалізації функції має бути застосовано метод Math.max() і apply().
+ */
 
-const myPrint = (a, b, res) => {
-  return `${a}^${b}=${res}`;
-};
+// const list = [12, 23, 100, 34, 56, 9, 233]
+// const myMax = () => {}
 
-const myPow = (a, b, myPrint) => {
-  let result;
+// console.log(myMax(list)); // 233
 
-  function pow(base, exp) {
-    if (exp === 0) return 1;
+/*
+ * #5
+ *
+ * Створіть функцію myMul(a, b), яка буде множити числа а і b, повертаючи результат.
+ */
 
-    if (exp > 0) {
-      return base * pow(base, exp - 1);
-    } else {
-      return 1 / pow(base, -exp);
-    }
-  }
+// const myMul = () => {}
 
-  result = pow(a, b);
+/*
+ * Створіть функції myDouble(n), яка приймає один параметр і подвоює його.
+ * Використовувати множення або інші математичні операції всередині функції - заборонено, тільки bind() і myMul().
+ * Функція повертає результат обчислення.
+ */
 
-  return myPrint(a, b, result);
-};
+// const myDouble
 
-console.log(myPow(3, 4, myPrint));
-console.log(myPow(2, 3, myPrint));
-console.log(myPow(2, 0, myPrint));
-console.log(myPow(2, -2, myPrint));
+// console.log(myDouble(3)) // = myMul(2, 3) = 6
+// console.log(myDouble(4)) // = myMul(2, 4) = 8
+// console.log(myDouble(5)) // = myMul(2, 5) = 10
 
-const list = [12, 23, 100, 34, 56, 9, 233];
+// Аналогічним чином створюємо функцію myTriple(n), яка потроює параметр, що приймає, повертаючи результат.
 
-const myMax = (arr) => {
-  return Math.max.apply(null, arr);
-};
+// const myTriple
 
-console.log(myMax(list));
+// console.log(myTriple(3)) // = myMul(3, 3) = 9
+// console.log(myTriple(4)) // = myMul(3, 4) = 12
+// console.log(myTriple(5)) // = myMul(3, 5) = 15
 
-const myMul = (a, b) => a * b;
-
-const myDouble = myMul.bind(null, 2);
-const myTriple = myMul.bind(null, 3);
-
-console.log(myDouble(3));
-console.log(myDouble(4));
-console.log(myDouble(5));
-
-console.log(myTriple(3));
-console.log(myTriple(4));
-console.log(myTriple(5));
+export { counter, counterFactory, myPow, myMax, myMul, myDouble, myTriple }
